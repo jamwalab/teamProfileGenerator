5b2124668a01e88e2d57b9255d28c193be9e64dd
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-25 02:02:49 -0400

    Employee class extended to Manager, Intern and Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,13 @@
-const Manager = require('../lib/Engineer');
+const Engineer = require('../lib/Engineer');
+
+test('Creates objects from Employee class', () => {
+    const engineer = new Engineer('AJ', 'TOR01', 'abc@gmail.com');
+
+    expect(engineer.name).toEqual('AJ');
+    expect(engineer.id).toEqual('TOR01');
+    expect(engineer.email).toEqual('abc@gmail.com');
+
+    expect(engineer.getName()).toEqual(engineer.name);
+    expect(engineer.getId()).toEqual(engineer.id);
+    expect(engineer.getEmail()).toEqual(engineer.email);
+})
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,13 @@
-const Manager = require('../lib/Intern');
+const Intern = require('../lib/Intern');
+
+test('Creates objects from Employee class', () => {
+    const intern = new Intern('AJ', 'TOR01', 'abc@gmail.com');
+
+    expect(intern.name).toEqual('AJ');
+    expect(intern.id).toEqual('TOR01');
+    expect(intern.email).toEqual('abc@gmail.com');
+
+    expect(intern.getName()).toEqual(intern.name);
+    expect(intern.getId()).toEqual(intern.id);
+    expect(intern.getEmail()).toEqual(intern.email);
+})
